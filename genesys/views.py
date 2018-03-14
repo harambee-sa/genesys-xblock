8adf196d8ae8dd0db5dd24681722f7140be89d53
lidijarad <radovanovic.lidija22@gmail.com>
2018-03-14 11:05:34 +0200

    convert indentation to spaces

--- genesys/views.py
+++ genesys/views.py
@@ -1,7 +1,3 @@
-
-
-
-
 import requests  
 import json
 import urlparse
@@ -13,22 +9,22 @@ from .models import GenesysData
 @csrf_exempt
 def genesys_result_receiver(request):
 
-	if request.method == 'POST':
-		referer = request.META.get('HTTP_REFERER')
-    	referer_parts = urlparse.urlparse(referer) if referer else None
-    	referer_hostname = referer_parts.hostname if referer_parts is not None else None
-    	domain_is_whitelisted = (
-        	referer_hostname in getattr(settings, 'CORS_ORIGIN_WHITELIST', [])
-    	)
-    	if not domain_is_whitelisted:
-    		return HttpResponseForbidden('Permission denied.')
-    	else:
-			received_json_data = json.loads(request.body)
-			GenesysData.objects.create(
-				event_type = received_json_data['eventType'],
-				event_date = received_json_data['eventDate'],
-				configuration_id = received_json_data['configurationId'],
-				respondent_id = received_json_data['respondantId'],
-				invitation_id = received_json_data['invitationId'],
-			)
-	return HttpResponse()
+    if request.method == 'POST':
+        referer = request.META.get('HTTP_REFERER')
+        referer_parts = urlparse.urlparse(referer) if referer else None
+        referer_hostname = referer_parts.hostname if referer_parts is not None else None
+        domain_is_whitelisted = (
+            referer_hostname in getattr(settings, 'CORS_ORIGIN_WHITELIST', [])
+        )
+        if not domain_is_whitelisted:
+            return HttpResponseForbidden('Permission denied.')
+        else:
+            received_json_data = json.loads(request.body)
+            GenesysData.objects.create(
+                event_type = received_json_data['eventType'],
+                event_date = received_json_data['eventDate'],
+                configuration_id = received_json_data['configurationId'],
+                respondent_id = received_json_data['respondantId'],
+                invitation_id = received_json_data['invitationId'],
+            )
+    return HttpResponse()
